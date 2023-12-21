732a01c96f0bfc2256acf926281b3bd2ce653a2c
neerajkumarroy <neerajkumarroyss@gmail.com>
2023-12-21 10:42:03 +0530

    add file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
-function App() {
+const App = () => {
+  // the code is automatically stop when we speak reset and 
+  const commands = [
+    {
+      command: "reset",
+      callback: ({ resetTranscript }) => resetTranscript()
+    },
+    {
+      command: 'open *',   // whatever website will we speak, that website will be open
+      callback: (site) => {
+        window.open('http://' + site);
+      }
+    },
+    {
+      // Change the background color of the browser whatever you speak
+      command: 'change background color to *',
+      callback: (color) => {
+        document.body.style.backgroundColor = color;  // Fix: Corrected the property assignment
+      }
+    }
+  ];
+
+  const { transcript, listening, resetTranscript, browserSupportsSpeechRecognition } = useSpeechRecognition({ commands });
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='div1'>
+      <p>Microphone: {listening ? 'on' : 'off'}</p>
+      <button onClick={() => SpeechRecognition.startListening({ continuous: true, language: 'en-IN' })}>Start</button>
+      <button onClick={SpeechRecognition.stopListening}>Stop</button>
+      <button onClick={resetTranscript}>Reset</button>
+      <p>{transcript}</p>
     </div>
   );
-}
+};
 
 export default App;
